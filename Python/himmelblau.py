606675e2129ef8577b5545a73d54963d0d06b105
OhIDunKnow <62465594+OhIDunKnow@users.noreply.github.com>
2022-03-30 22:17:56 +0800

    updated himmel

--- Python/himmelblau.py
+++ Python/himmelblau.py
@@ -1,28 +1,26 @@
-global hessian_vecshaped 
-hessian_vecshaped = [0,0,0,0]
-global grad
-grad = [0,0]
+# global hessian_vecshaped 
+# hessian_vecshaped = [0,0,0,0]
+# global grad
+# grad = [0,0]
 
-# def valueonly_himmelblau2d(dim, x):
-#     if (dim != 2):
-#         print("Dim is not 2")
+def valueonly_himmelblau(dim, x):
+    if (dim != 2):
+        print("Dim is not 2")
 
-#     p1 = 2 * (x[0] * x[0] + x[1] - 11)
-#     p2 = 2 * (x[0] + x[1] * x[1] - 7)
+    p1 = (x[0] * x[0] + x[1] - 11)
+    p2 = (x[0] + x[1] * x[1] - 7)
 
+    ret = p1*p1 + p2*p2
 
-#     # p3 = 2.625 - x[0] + x[0]*x[1]*x[1]*x[1]
-
-#     ret = p1*p1 + p2*p2 + p3*p3
-
-#     return ret
+    return ret
 
 
-def valueandderivatives_himmelblau2d(dim, x):
+def valueandderivatives_himmelblau(dim, x):
     if (dim != 2):
         print("dim is not 2")
         exit(2)
 
+    ret = valueonly_himmelblau(dim, x)
 
     # gradient
     p1 = 2 * (x[0] * x[0] + x[1] - 11)
@@ -31,17 +29,18 @@ def valueandderivatives_himmelblau2d(dim, x):
     grad[0] = 2 * p1 * 2 * x[0] + 2 * p2
     grad[1] = 2 * p1 + 2 * p2 * 2 * x[1]
 
-    hessian_vecshaped[0+2*0] = 4 * x[0] * 2 * x[0] + 2 * p1 * 2 + 2
-    hessian_vecshaped[1+2*1] = 2 + 2 * p2 * 2 + 4 * x[1] * 2 * x[1]
-    hessian_vecshaped[1+2*0] = 4 * x[0] + 4 * x[1]
+    hessian_vecshaped[0+2*0] = (4 * x[0] * 2 * x[0]) + (2 * p1 * 2) + 2
+    hessian_vecshaped[1+2*1] = 2 + (2 * p2 * 2) + (4 * x[1] * 2 * x[1])
+    hessian_vecshaped[1+2*0] = (4 * x[0]) + (4 * x[1])
     hessian_vecshaped[0+2*1] = hessian_vecshaped[1+2*0]; 
     
-    #return ret
+    return ret
 
-x = [-5, 5]
+# x = [-5, 5]
 
-for i in range (-5, 5, 1):
-    x = [i, i+1]
-    valueandderivatives_himmelblau2d(2, x)
-    print(hessian_vecshaped)
-    print(grad)
+# for i in range (-5, 5, 1):
+#     x = [i, i+1]
+#     double = valueandderivatives_himmelblau(2, x)
+#     print(double)
+#     print(hessian_vecshaped)
+#     print(grad)
